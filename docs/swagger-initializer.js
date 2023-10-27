ead255f962b2f8158bf5b8df17f78a6f904a5254
MayarAlsalem01 <mayaralsalem21@gmail.com>
2023-10-27 15:49:18 +0200

    add docs and updated

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -4,6 +4,7 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url: "http://simple-ecommerce.somee.com/swagger/v1/swagger.json",
+    
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
